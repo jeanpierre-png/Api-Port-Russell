47a9d0141ed8fff03792c246651f35c7daf0cca9
NOZA Jean-Pierre <nozajean-pierre@MacBook-Air-de-Jean-Pierre.local>
2026-02-23 17:28:51 -0300

    Créaction du model Catway

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const app = express();
 app.use(express.json());
 
 app.use('/auth', require('./routes/auth.routes'));
+app.use('/catways', require('./routes/catway.routes'));
 
 module.exports = app;
