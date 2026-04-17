a92897efc86bb14baf5cf557ae24fd565776f02e
NOZA Jean-Pierre <nozajean-pierre@MacBook-Air-de-Jean-Pierre.local>
2026-04-17 09:32:19 -0300

    déploiement sur Heroku

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ require('dotenv').config();
 const app = require('./app');
 const connectDB = require('./config/database');
 
-connectDB();
+const startServer = async () => {
+  try {
+    await connectDB();
 
-const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Serveur lancé sur le port ${PORT}`);
-});
+    app.listen(PORT, () => {
+    console.log(`Serveur lancé sur le port ${PORT}`);
+  });
+
+  } catch (error) {
+    console.error('Erreur au démarrage du serveur:', error);
+    process.exit(1);
+  }
+
+};
+
+startServer();
